9cad5157920e9f118539d80389d17df7add0fefd
Vedant Chakkarwar <vedantchakkarwar@gmail.com>
2024-07-31 11:56:02 +0530

    Remove_Duplicates_From_Array

---
+++ 26_remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int>s;
+        for(auto& num:nums){
+            s.insert(num);
+        }
+        nums = vector<int>(s.begin(),s.end());
+        return nums.size();
+    }
+};
